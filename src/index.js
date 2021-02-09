b09810d11a5c741d18b279a51c6872ec9a84d0ef
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-09 11:56:31 -0600

    Added images to website with some css styling to make sure they are all the same size

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading">My Favourite Team</h1>
+    <div>
+      <img
+        src="https://s.yimg.com/cv/apiv2/default/20181022/500x500/astros_wbg.png"
+        alt=""
+      ></img>
+      <img
+        src="https://cdn.vox-cdn.com/thumbor/453sbf8-nEDdC8pz8R1ijyLLBt4=/0x0:1200x800/1200x800/filters:focal(504x304:696x496)/cdn.vox-cdn.com/uploads/chorus_image/image/66118151/baumann_reaches_of_astros_getty_ringer.0.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Houston_Astros_cap_logo.svg/1200px-Houston_Astros_cap_logo.svg.png"
+        alt=""
+      ></img>
+    </div>
   </div>,
   document.getElementById("root")
 );
