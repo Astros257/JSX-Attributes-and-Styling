686b0fb704ba11f2d831c6be42cdef315ec6240d
Astros257 <48536928+Astros257@users.noreply.github.com>
2021-02-09 12:06:01 -0600

    how to add js to image attrivutes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading">My Favourite Team</h1>
     <div>
-      <img
-        src="https://s.yimg.com/cv/apiv2/default/20181022/500x500/astros_wbg.png"
-        alt=""
-      ></img>
-      <img
-        src="https://cdn.vox-cdn.com/thumbor/453sbf8-nEDdC8pz8R1ijyLLBt4=/0x0:1200x800/1200x800/filters:focal(504x304:696x496)/cdn.vox-cdn.com/uploads/chorus_image/image/66118151/baumann_reaches_of_astros_getty_ringer.0.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Houston_Astros_cap_logo.svg/1200px-Houston_Astros_cap_logo.svg.png"
-        alt=""
-      ></img>
+      <img src={img + "?grayscale"} alt="radnom" />
     </div>
   </div>,
   document.getElementById("root")
